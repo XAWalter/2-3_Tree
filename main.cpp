0a7fe3056219e3264663d7f56e7c03860c0cd3be
XAWalter <33841055+XAWalter@users.noreply.github.com>
2019-02-16 14:27:02 -0800

    Fixed insert

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "Tree.h"
+
+using namespace std;
+
+int main() {
+	Tree test;
+
+	test.insert(10);
+	test.insert(1);
+	test.insert(12);
+	test.insert(3);
+	test.insert(5);
+	test.insert(11);
+	test.insert(13);
+}
